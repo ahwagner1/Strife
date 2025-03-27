849e50ee68879b8893e89006e53fd3c30233fd02
ahwagner1 <wagner.ashton04@gmail.com>
2025-03-26 19:30:27 -0800

    Created a simple logger utility class to help me debug in the future

---
+++ util/logger.cpp
@@ -0,0 +1,42 @@
+#include "logger.hpp"
+
+Logger::Logger(const std::string& filename) : m_logFile(filename, std::ios::app) {
+    if (!m_logFile.is_open()) {
+        std::cerr << "Unable to open log file" << std::endl;
+    }
+}
+
+Logger::~Logger() {
+    if (m_logFile.is_open()) {
+        m_logFile.close();
+    }
+}
+
+void Logger::log(LogLevel level, const std::string& message) {
+    std::lock_guard<std::mutex> guard(m_logMutex);
+    m_logFile << "[" << currentDateTime() << "]" << "[" << logLevelToString(level) << "] " << message << std::endl; 
+}
+
+std::string Logger::currentDateTime() {
+    std::time_t now = std::time(nullptr);
+    char buffer[80];
+
+    std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %X", std::localtime(&now));
+    return buffer;
+}
+
+std::string Logger::logLevelToString(LogLevel level) {
+    switch(level) {
+        case LogLevel::INFO:
+            return "INFO";
+            break;
+        case LogLevel::WARNING:
+            return "WARNING";
+            break;
+        case LogLevel::ERROR:
+            return "ERROR";
+            break;
+        default:
+            return "UNKNOWN";
+    }
+}
---
+++ util/logger.hpp
@@ -0,0 +1,28 @@
+#ifndef STRIFE_UTIL_LOGGER_H
+#define STRIFE_UTIL_LOGGER_H
+
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <string>
+#include <mutex>
+
+
+class Logger {
+
+public:
+    enum LogLevel {INFO, WARNING, ERROR};
+
+    Logger(const std::string& filename);
+    ~Logger();
+    void log(LogLevel level, const std::string& message);
+
+private:
+    std::ofstream m_logFile;
+    std::mutex m_logMutex;
+
+    std::string currentDateTime();
+    std::string logLevelToString(LogLevel level);
+};
+
+#endif
